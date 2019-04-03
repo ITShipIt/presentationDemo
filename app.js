773eeb3401963ed075bb7e25477bf8ff789cfa73
Trevor Garn <trevash77@gmail.com>
2019-04-03 16:45:11 -0600

    First commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+
+const port = 80;
+
+const app = new express();
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req, res) {
+  console.log('Got to url');
+  res.sendFile(path.join(__dirname + '/index.html'));
+})
+
+app.listen(port);
+
+console.log("Running on port:", port);
